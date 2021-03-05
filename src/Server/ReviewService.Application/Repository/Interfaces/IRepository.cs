587863eee60c1e662276f1e3cf3c395fcd2a73db
Admin <Admin@DESKTOP-1RSB9I9>
2021-03-05 14:20:20 +0200

    Imolemented generic repository

--- src/Server/ReviewService.Application/Repository/Interfaces/IRepository.cs
+++ src/Server/ReviewService.Application/Repository/Interfaces/IRepository.cs
@@ -7,7 +7,10 @@ namespace ReviewService.Application.Repository.Interfaces
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        Task<TEntity> GetByIdAsync(object id);
+        Task<TEntity> GetByIdAsync(int id);
         Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> searchExpression = null);
+        Task CreateAsync(TEntity item);
+        Task DeleteAsync(TEntity item);
+        Task UpdateAsync(TEntity item);
     }
 }
