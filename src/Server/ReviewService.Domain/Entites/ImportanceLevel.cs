d2f3ac58a66bc7ed98411fa0aba812addaa6077e
Admin <Admin@DESKTOP-1RSB9I9>
2021-03-05 07:39:51 +0200

    Added EvaluationPoint, ImportanceLevel entities

---
+++ src/Server/ReviewService.Domain/Entites/ImportanceLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace ReviewService.Domain.Entites
+{
+    public class ImportanceLevel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+    }
+}
