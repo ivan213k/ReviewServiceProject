4056e87a34e829a8eded9f327495ab02eebd4753
Admin <Admin@DESKTOP-1RSB9I9>
2021-03-08 10:08:06 +0200

    Added evaluation point service

---
+++ src/Server/ReviewService.Application/Areas/Interfaces/IEvaluationPointService.cs
@@ -0,0 +1,12 @@
+﻿using ReviewService.Domain.Entites;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReviewService.Application.Areas.Interfaces
+{
+    public interface IEvaluationPointService
+    {
+        Task<List<EvaluationPoint>> GetEvaluationPointsAsync();
+        Task AddEvaluationPointAsync(EvaluationPoint evaluationPoint);
+    }
+}
