08fcd3878a1c6506884a8dd80ff37ff3f7fafb25
ivan213k <ivan213k@gmail.com>
2021-03-12 09:47:47 +0200

    Implemented review session service

---
+++ src/Server/ReviewService.Application/ReviewSessions/Interfaces/IReviewSessionService.cs
@@ -0,0 +1,15 @@
+﻿using ReviewService.Domain.Entites;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReviewService.Application.ReviewSessions.Interfaces
+{
+    public interface IReviewSessionService
+    {
+        Task<List<ReviewSession>> GetReviewSessionsAsync();
+        Task CreateReviewSessionAsync(ReviewTemplate template, ReviewSession reviewSession);
+        Task DeleteReviewSessionAsync(ReviewSession reviewSession);
+        Task PublishReviewSessionAsync(ReviewSession reviewSession);
+        Task CancelReviewSessionAsync(ReviewSession reviewSession);
+    }
+}
