0463a2224525da472c61111939da01e94a8ab386
ivan213k <ivan213k@gmail.com>
2021-03-15 16:56:12 +0200

    Implemented importance level controller

---
+++ src/Server/ReviewService.Web.Server/AutoMapperProfiles/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReviewService.Domain.Entites;
+using ReviewService.Shared.ApiModels;
+
+namespace ReviewService.Web.Server.AutoMapperProfiles
+{
+    class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ImportanceLevel, ImportanceLevelApiModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/Server/ReviewService.Web.Server/Controllers/ImportanceLevelController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ReviewService.Application.ImportanceLevels.Interfaces;
+using ReviewService.Domain.Entites;
+using ReviewService.Shared.ApiModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewService.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportanceLevelController : ControllerBase
+    {
+        private readonly IImportanceLevelService _importanceLevelService;
+        private readonly IMapper _autoMapper;
+
+        public ImportanceLevelController(IImportanceLevelService importanceLevelService, IMapper autoMapper)
+        {
+            _importanceLevelService = importanceLevelService;
+            _autoMapper = autoMapper;
+        }
+
+        [HttpGet]
+        public async Task<List<ImportanceLevelApiModel>> GetImportanceLevels()
+        {
+            var importanceLevels = await _importanceLevelService.GetImportanceLevelsAsync();
+            return _autoMapper.Map<List<ImportanceLevelApiModel>>(importanceLevels);
+        }
+
+        [HttpPost]
+        public async Task AddImportanceLevel([FromBody] ImportanceLevelApiModel importanceLevelApiModel)
+        {
+            var importanceLevel = _autoMapper.Map<ImportanceLevel>(importanceLevelApiModel);
+            await _importanceLevelService.AddImportanceLevelAsync(importanceLevel);
+        }
+
+        [HttpDelete]
+        public async Task DeleteImportanceLevel(int id)
+        {
+            var importanceLevels = await _importanceLevelService.GetImportanceLevelsAsync();
+            var importanceLevel = importanceLevels.FirstOrDefault(r => r.Id == id);
+            if (importanceLevel is null)
+            {
+                return;
+            }
+            await _importanceLevelService.DeleteImportanceLevelAsync(importanceLevel);
+        }
+    }
+}
