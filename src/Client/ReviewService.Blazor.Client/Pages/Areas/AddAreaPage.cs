5af7f5d610531a2f9425009f12a78ca357a4cf68
ivan213k <ivan213k@gmail.com>
2021-03-18 19:57:47 +0200

    Implemented areas page

---
+++ src/Client/ReviewService.Blazor.Client/Pages/Areas/AddAreaPage.cs
@@ -0,0 +1,19 @@
+﻿using ReviewService.Shared.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewService.Blazor.Client.Pages.Areas
+{
+    public partial class AddAreaPage
+    {
+        private AreaApiModel area;
+
+        public AddAreaPage()
+        {
+            area = new AreaApiModel();
+            area.AreaItems = new List<AreaItemApiModel>();
+        }
+    }
+}
---
+++ src/Client/ReviewService.Blazor.Client/Pages/Areas/AreasPage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using ReviewService.Shared.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ReviewService.Blazor.Client.Pages.Areas
+{
+    public partial class AreasPage
+    {
+        private List<AreaApiModel> areas;
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+        
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            areas = await HttpClient.GetFromJsonAsync<List<AreaApiModel>>("api/Area");
+        }
+
+        private void OnAddNewAreaClicked() 
+        {
+            NavigationManager.NavigateTo("/addArea");
+        }
+    }
+}
