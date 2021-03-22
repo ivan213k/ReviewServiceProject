e9a4e15465e6c0eb73d55eb8a8fdf025decf7c47
ivan213k <ivan213k@gmail.com>
2021-03-22 17:42:57 +0200

    Implemented review templates page

---
+++ src/Client/ReviewService.Blazor.Client/Pages/ReviewTemplates/ReviewTemplatesPage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using ReviewService.Shared.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ReviewService.Blazor.Client.Pages.ReviewTemplates
+{
+    public partial class ReviewTemplatesPage
+    {
+        private List<ReviewTemplateApiModel> reviewTemplates;
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            reviewTemplates = await HttpClient.GetFromJsonAsync<List<ReviewTemplateApiModel>>("api/ReviewTemplate");
+        }
+    }
+}
