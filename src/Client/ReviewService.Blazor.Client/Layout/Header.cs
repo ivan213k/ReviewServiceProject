7e9c075f7851978087552b6d636f7b7fa2619c3d
Yuliia-077 <62853554+Yuliia-077@users.noreply.github.com>
2021-03-25 00:36:53 +0200

    Changed layout and importance levels ui

---
+++ src/Client/ReviewService.Blazor.Client/Layout/Header.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using ReviewService.Shared.ApiModels;
+using ReviewService.Blazor.Client.Components;
+
+namespace ReviewService.Blazor.Client.Layout
+{
+    public partial class Header
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        private bool _drawerOpen;
+
+        private void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+    }
+}
