d4dac15bba3b25cfd14c8eac0817282a20e80ec2
ivan213k <ivan213k@gmail.com>
2021-03-25 06:52:08 +0200

    Added mid evaluation point property to review template

---
+++ src/Server/ReviewService.Infrastructure/Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ReviewService.Infrastructure.Persistance
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ReviewServiceDbContext>
+    {
+        public ReviewServiceDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ReviewServiceDbContext>();
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("dbsettings.json");
+            var config = builder.Build();
+            
+            string connectionString = config.GetConnectionString("LocalConnection");
+            optionsBuilder.UseSqlServer(connectionString, opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+            return new ReviewServiceDbContext(optionsBuilder.Options);
+        }
+    }
+}
