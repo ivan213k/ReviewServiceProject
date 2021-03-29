c6c21ab2e1887040997e5547933cc3b5d61ed5e4
ivan213k <ivan213k@gmail.com>
2021-03-29 12:35:52 +0300

    Updated add review template page to mudBlazor

--- src/Client/ReviewService.Blazor.Client/Pages/ReviewTemplates/AddReviewTemplatePage.cs
+++ src/Client/ReviewService.Blazor.Client/Pages/ReviewTemplates/AddReviewTemplatePage.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.Forms;
 using ReviewService.Blazor.Client.Layout;
 using ReviewService.Shared.ApiModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -17,13 +18,14 @@ namespace ReviewService.Blazor.Client.Pages.ReviewTemplates
         private Header header;
         private List<EvaluationPointsTemplateApiModel> evaluationPointsTemplates;
         private List<EvaluationPointApiModel> evaluationPoints;
+        private List<AreaApiModel> areas;
 
         [Parameter]
         public int? Id { get; set; }
 
         [Inject]
         public HttpClient HttpClient { get; set; }
-       
+
         [Inject]
         public NavigationManager NavigationManager { get; set; }
         public AddReviewTemplatePage()
@@ -43,25 +45,22 @@ namespace ReviewService.Blazor.Client.Pages.ReviewTemplates
                 evaluationPoints = evaluationPointsTemplates.Find(r => r.Id == reviewTemplate.EvaluationPointsTemplateId).EvaluationPoints;
             }
         }
-
-        private void DeleteAreaRow(AreaApiModel area)
-        {
-            reviewTemplate.Areas.Remove(area);
-        }
+       
         private void AddAreaRow(AreaApiModel area)
         {
-            if (reviewTemplate.Areas.Any(a=>a.Name == area.Name))
+            if (reviewTemplate.Areas.Any(a => a.Name == area.Name))
             {
                 return;
             }
             reviewTemplate.Areas.Add(area);
             StateHasChanged();
         }
-        private void OnCancelClicked()
+        private void DeleteAreaRow(AreaApiModel area)
         {
-            NavigationManager.NavigateTo("/reviewTemplates");
+            reviewTemplate.Areas.Remove(area);
         }
-        private async void OnSaveClicked() 
+        
+        private async void OnSaveClicked()
         {
             if (editForm.EditContext.Validate())
             {
@@ -74,20 +73,43 @@ namespace ReviewService.Blazor.Client.Pages.ReviewTemplates
                     await HttpClient.PutAsJsonAsync("api/ReviewTemplate", reviewTemplate);
                 }
                 NavigateToReviewTemplates();
-            }        
-        }
-        private void OnEvaluationPointTemplateSelectionChanged(ChangeEventArgs args)
-        {
-            if (int.TryParse(args.Value.ToString(), out int pointTemplateId))
-            {
-                evaluationPoints = evaluationPointsTemplates.Find(r => r.Id == pointTemplateId).EvaluationPoints;
-                reviewTemplate.MidEvaluationPointId = evaluationPoints.FirstOrDefault().Id;
             }
+        }
+        private void OnCancelClicked()
+        {
+            NavigationManager.NavigateTo("/reviewTemplates");
+        }
+        private void OnEvaluationPointTemplateSelectionChanged(int pointTemplateId)
+        {
+            evaluationPoints = evaluationPointsTemplates.Find(r => r.Id == pointTemplateId).EvaluationPoints;
+            reviewTemplate.EvaluationPointsTemplateId = pointTemplateId;
+            reviewTemplate.MidEvaluationPointId = evaluationPoints.FirstOrDefault().Id;
         }
 
         private void NavigateToReviewTemplates()
         {
             NavigationManager.NavigateTo("/reviewTemplates");
+        }
+        private async Task<List<AreaApiModel>> LoadAreas()
+        {
+            return await HttpClient.GetFromJsonAsync<List<AreaApiModel>>("api/Area");
+        }
+        private async Task<IEnumerable<string>> SearchAreas(string value)
+        {
+            areas = await LoadAreas();
+
+            if (string.IsNullOrEmpty(value))
+                return areas.Select(a => a.Name).ToList();
+            return areas.Where(a => a.Name.Contains(value, StringComparison.InvariantCultureIgnoreCase)).Select(a=>a.Name);
+        }
+        private void SearchAreaValueChanged(string value)
+        {
+            var selectedArea = areas.FirstOrDefault(a => a.Name == value);
+            if (selectedArea is null)
+            {
+                return;
+            }
+            AddAreaRow(selectedArea);
         }
     }
 }
