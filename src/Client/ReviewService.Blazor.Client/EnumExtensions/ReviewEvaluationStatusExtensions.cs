3f1b6b9db7d427cc8ad3a4c5638407747c3b1432
ivan213k <ivan213k@gmail.com>
2021-04-06 10:31:33 +0300

    Added displaying session status

---
+++ src/Client/ReviewService.Blazor.Client/EnumExtensions/ReviewEvaluationStatusExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ReviewService.Shared.ApiEnums;
+
+namespace ReviewService.Blazor.Client.EnumExtensions
+{
+    public static class ReviewEvaluationStatusExtensions
+    {
+        public static string ToUserFriendlyString(this ReviewEvaluationStatusApiEnum reviewEvaluationStatus)
+        {
+            switch (reviewEvaluationStatus)
+            {
+                case ReviewEvaluationStatusApiEnum.NotStarted:
+                    return "Not started";
+                case ReviewEvaluationStatusApiEnum.InProgress:
+                    return "In progress";
+                case ReviewEvaluationStatusApiEnum.Finished:
+                    return "Finished";
+                default:
+                    return reviewEvaluationStatus.ToString();
+            }
+        }
+    }
+}
---
+++ src/Client/ReviewService.Blazor.Client/EnumExtensions/ReviewSessionStatusExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ReviewService.Shared.ApiEnums;
+
+namespace ReviewService.Blazor.Client.EnumExtensions
+{
+    public static class ReviewSessionStatusExtensions
+    {
+        public static string ToUserFriendlyString(this ReviewSessionStatusApiEnum sessionStatus) 
+        {
+            switch (sessionStatus)
+            {
+                case ReviewSessionStatusApiEnum.New:
+                    return "New Review Session";
+                case ReviewSessionStatusApiEnum.Published:
+                    return "Review Published";
+                case ReviewSessionStatusApiEnum.InProgress:
+                    return "In Progress";
+                case ReviewSessionStatusApiEnum.Submitted:
+                    return "Review Submitted";
+                case ReviewSessionStatusApiEnum.Canceled:
+                    return "Review Canceled";
+                default:
+                    return sessionStatus.ToString();
+            }
+        }
+    }
+}
