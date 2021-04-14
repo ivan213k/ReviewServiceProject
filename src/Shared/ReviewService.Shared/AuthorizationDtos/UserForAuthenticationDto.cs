256dafd6d025846a424be0354c45e6ca3cb7cc67
ivan213k <ivan213k@gmail.com>
2021-04-14 11:17:01 +0300

    Implemented jwt tokens authentication

---
+++ src/Shared/ReviewService.Shared/AuthorizationDtos/UserForAuthenticationDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewService.Shared.AuthorizationDtos
+{
+    public class UserForAuthenticationDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
